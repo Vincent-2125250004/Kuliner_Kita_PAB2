8c30b447dbd1c6c3dd7e6b40a710822e78964c4e
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-04-05 09:22:42 +0700

    05/04/2023

---
+++ app/src/main/java/com/if4a/myapplication/Model/ModelResponse.java
@@ -0,0 +1,21 @@
+package com.if4a.myapplication.Model;
+
+import java.util.List;
+
+public class ModelResponse {
+    private String kode, pesan;
+    private List<ModelKuliner> data;
+    
+    public List<ModelKuliner> getData() {
+        return data;
+    }
+
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+}
