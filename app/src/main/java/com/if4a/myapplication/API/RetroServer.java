6a3a48003cebef89fad4db0128e7c4148fa4d6e0
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-04-17 09:21:52 +0700

    Update 17/04/2023

---
+++ app/src/main/java/com/if4a/myapplication/API/RetroServer.java
@@ -0,0 +1,19 @@
+package com.if4a.myapplication.API;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroServer {
+
+    private static final String BASE_URL = "https://kulinervincent.000webhostapp.com/";
+
+    private static Retrofit retro;
+
+    public static Retrofit konekRetrofit() {
+        if (retro == null) {
+            retro = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retro;
+    }
+
+}
